9b9eec670c8c20e02d76da19e875abb522f6542e
WALTER <waltergutierrezl123@gmail.com>
2025-02-25 11:37:21 -0500

    Hay un error en el blazor, no quiere coger las propiedades de la lista

--- Entidades/Entities/Prestamo.cs
+++ Entidades/Entities/Prestamo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace Entidades.Entities
 {
     public class Prestamo
     {
+        [Key]
+        public int PrestamoID {  get; set; }
+        public int LibroID { get; set; }
+        public int UsuarioID { get; set; }
+        public DateOnly FechaPrestamo { get; set; }
+        public DateOnly FechaDevolucion { get; set; }
+        public string? Estado { get; set; }
     }
 }
